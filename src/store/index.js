95ce8bffbcf978965a612852a214ee9d5f1728fe
xuqianchao <2750833015@qq.com>
2022-08-25 16:18:51 +0800

    base-ui

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+import mutations from './mutations'
+import actions from './actions'
 
 Vue.use(Vuex)
 
+const debug = process.env.NODE_ENV !== 'production'
+const vuexlocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
   state: {
+    loading: false,
+    sending: false,
+    error: null,
+    user: [],
+    reconnect: false,
+    activeRoom: null,
+    rooms: [],
+    users: [],
+    messages: [],
+    userTyping: null
   },
   getters: {
+    hasError: state => state.error? true : false
   },
-  mutations: {
-  },
-  actions: {
-  },
+  mutations,
+  actions, 
   modules: {
-  }
+  },
+  //plugins: [vuexlocal.plugin],
+  //strict: debug
 })
